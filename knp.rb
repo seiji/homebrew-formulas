be25de0e77caba7f152353c98db9207eedb9bfbe
Seiji Toyama <toyama.seiji@gmail.com>
2017-10-11 17:25:04 +0900

    Add knp formula

---
+++ knp.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+
+class Knp < Formula
+  desc ""
+  homepage "http://nlp.ist.i.kyoto-u.ac.jp/index.php?KNP"
+  url "http://nlp.ist.i.kyoto-u.ac.jp/DLcounter/lime.cgi?down=http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/knp/knp-4.18.tar.bz2&name=knp-4.18.tar.bz2"
+  sha256 "4205ad0d5f2c5174879c7f23990064e531bb170365bf05e75bd683d7c5fe3d4b"
+  depends_on "juman"
+  depends_on "tinycdb"
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --with-juman-prefix=#{HOMEBREW_PREFIX}/opt/juman
+    ]
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test knp`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
