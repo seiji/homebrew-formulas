31f1aea4e5eb06674ed66cbfe5cde65405fca0b0
seiji <toyama.seiji@gmail.com>
2019-09-25 19:28:59 +0900

    Update formula

--- aws-sessionmanager.rb
+++ aws-sessionmanager.rb
@@ -4,29 +4,14 @@ class AwsSessionmanager < Formula
   url "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip"
   sha256 "a50e31e8cf92c279c3cce01fe0ffe3df1f93134c29ef69315c1cc229e01ffdbb"
 
-  # depends_on "cmake" => :build
+  depends_on "awscli"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "bin/session-manager-plugin"
+    prefix.install %w[LICENSE VERSION]
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test sessionmanager`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system bin/"session-manager-plugin"
   end
 end
