5af3c63673d3ddf44c178e246e24bdbb2070428a
seiji <toyama.seiji@gmail.com>
2019-09-26 12:36:18 +0900

    Set aws session manager version

--- aws-sessionmanager.rb
+++ aws-sessionmanager.rb
@@ -1,8 +1,8 @@
 class AwsSessionmanager < Formula
   desc "Session Manager Plugin for the AWS CLI"
   homepage "https://docs.aws.amazon.com/en_pv/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
-  url "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip"
-  sha256 "a50e31e8cf92c279c3cce01fe0ffe3df1f93134c29ef69315c1cc229e01ffdbb"
+  url "https://s3.amazonaws.com/session-manager-downloads/plugin/1.1.33.0/mac/sessionmanager-bundle.zip"
+  sha256 "ce09c71b26f4b2e952dcec7d335e887b2b8e6f8bf1528c996c94c825f15f7a99"
 
   depends_on "awscli"
 
