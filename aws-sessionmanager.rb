0df1c9023c8a8e69e5492480576b1a9563dd35bb
seiji <toyama.seiji@gmail.com>
2019-12-16 12:13:37 +0900

    Bumpup version of aws-sessionmanager

--- aws-sessionmanager.rb
+++ aws-sessionmanager.rb
@@ -1,9 +1,9 @@
 class AwsSessionmanager < Formula
   desc "Session Manager Plugin for the AWS CLI"
   homepage "https://docs.aws.amazon.com/en_pv/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
-  url "https://s3.amazonaws.com/session-manager-downloads/plugin/1.1.33.0/mac/sessionmanager-bundle.zip"
-  sha256 "ce09c71b26f4b2e952dcec7d335e887b2b8e6f8bf1528c996c94c825f15f7a99"
-  version "1.1.33.0"
+  url "https://s3.amazonaws.com/session-manager-downloads/plugin/1.1.50.0/mac/sessionmanager-bundle.zip"
+  sha256 "e95c672610f8f28fb13f67551c3f0da77d7ab7b5669670e82fd1c57a15e6bd09"
+  version "1.1.50.0"
 
   depends_on "awscli"
 
