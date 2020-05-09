115e3004dce12dc6044cd0367d36556007a17d5e
seiji <toyama.seiji@gmail.com>
2020-05-09 21:59:41 +0900

    Rename cask

--- Casks/aws-vault-beta.rb
+++ Casks/aws-vault-beta.rb
@@ -1,9 +1,8 @@
-cask 'aws-vault' do
+cask 'aws-vault-beta' do
   version '6.0.0-beta4'
   sha256 '4ea3442c378fc24caeba5886cec6a13829cfa00271aa65ec4071202dd824235f1'
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
-  appcast 'https://github.com/99designs/aws-vault/releases.atom'
   name 'aws-vault'
   homepage 'https://github.com/99designs/aws-vault'
 
