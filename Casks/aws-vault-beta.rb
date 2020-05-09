1f6a19e2e34de5e17d254a4645a8609da4a8c7ce
seiji <toyama.seiji@gmail.com>
2020-05-09 22:00:55 +0900

    Fix cask name

--- Casks/aws-vault-beta.rb
+++ Casks/aws-vault-beta.rb
@@ -3,7 +3,7 @@ cask 'aws-vault-beta' do
   sha256 '4ea3442c378fc24caeba5886cec6a13829cfa00271aa65ec4071202dd824235f1'
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
-  name 'aws-vault'
+  name 'aws-vault-beta'
   homepage 'https://github.com/99designs/aws-vault'
 
   binary 'aws-vault'
