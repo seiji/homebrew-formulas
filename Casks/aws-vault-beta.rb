1c2530edc127a9c839c2a12874b6dfb7ec718dfd
seiji <toyama.seiji@gmail.com>
2020-05-09 22:02:08 +0900

    Fix hash of aws-vault-beta

--- Casks/aws-vault-beta.rb
+++ Casks/aws-vault-beta.rb
@@ -1,6 +1,6 @@
 cask 'aws-vault-beta' do
   version '6.0.0-beta4'
-  sha256 '4ea3442c378fc24caeba5886cec6a13829cfa00271aa65ec4071202dd824235f1'
+  sha256 'ea3442c378fc24caeba5886cec6a13829cfa00271aa65ec4071202dd824235f1'
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
   name 'aws-vault-beta'
