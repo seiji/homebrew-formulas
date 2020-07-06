9b8006f7c882f7f90ac4b326aece1834c34411d0
Seiji Toyama <toyama.seiji@gmail.com>
2020-07-06 13:43:02 +0900

    Upgrade aws-vault

--- Casks/aws-vault-beta.rb
+++ Casks/aws-vault-beta.rb
@@ -1,6 +1,6 @@
 cask 'aws-vault-beta' do
-  version '6.0.0-beta4'
-  sha256 'ea3442c378fc24caeba5886cec6a13829cfa00271aa65ec4071202dd824235f1'
+  version '6.0.0-beta5'
+  sha256 '9f5d1ff8e38cdf86d0a36c983370a908e672373fb75af53d2f4cecdb49830a02'
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
   name 'aws-vault-beta'
