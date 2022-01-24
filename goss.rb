3a4987c8b025f74380045edb68af6ff209196edb
Seiji Toyama <toyama.seiji@gmail.com>
2022-01-24 15:44:01 +0900

    Add goss

---
+++ goss.rb
@@ -0,0 +1,19 @@
+class Goss < Formula
+  desc "Quick and Easy server testing/validation"
+  homepage "https://goss.rocks"
+  url "https://github.com/aelsabbahy/goss/archive/refs/tags/v0.3.16.tar.gz"
+  sha256 "8c17a59ba849a55283169589a3d46a922ee52f65d021411d59971b1b073e244a"
+  license "Apache-2.0"
+  head "https://github.com/aelsabbahy/goss.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system bin/"goss", "--version"
+  end
+end
